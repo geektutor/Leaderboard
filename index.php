5cf6251f85648268396382b717bc4283c862660b
Emmanuerl <emekaemmanuel045@gmail.com>
2020-03-25 16:15:18 +0100

    user dashboard fix

--- index.php
+++ index.php
@@ -3,7 +3,11 @@ require "./config/connect.php";
 require "./config/core.php";
 
 if (logged_in()) {
-  header("location: ./includes/leaderboard.php");
+  if ($_SESSION['isAdmin'] === true) {
+    header("location: ./dashboard/admin/index.php");
+  }else {
+    header("location: ./dashboard/user/index.php");
+  } 
 }else if(!logged_in()) {
   header("location: login.php");
 }
