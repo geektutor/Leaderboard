76a3cb4b5294032086668511ebf1dac0a8128d52
geektutor <sodiq.akinjobi@gmail.com>
2020-03-26 19:07:09 +0100

    Function

--- function.php
+++ function.php
@@ -1,6 +1,5 @@
 <?php include('config/connect.php'); 
-//i don't know what i am writing tho, i hope it works sha
-    function total_score($email){   
+
         global $conn;
         $queryURL = "SELECT points FROM submissions WHERE user = $email ";
         $resultURL = mysqli_query($conn, $queryURL);
@@ -10,15 +9,15 @@
             while($row = $resultURL->fetch_assoc()) {
                 $total += $row['points'];
             }
-            $email = $_SESSION['login_user'];
-            $total = total_score($email);
-            $sql = "UPDATE `user` SET score = $total WHERE email = $email";
-            $result = mysqli_query($conn, $sql);
+            return $total;
         }
         else{
             return $total;
         }
-    }
+        $email = $_SESSION['login_user'];
+        $total = total_score($email);
+        $sql = "UPDATE `user` SET score = $total WHERE email = $email";
+        $result = mysqli_query($conn, $sql);
     //do the rest, i'm blank
 ?>
 
