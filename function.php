c0950843926a952a8a356efb76a2c93ef5ff8e97
geektutor <sodiq.akinjobi@gmail.com>
2020-03-26 19:22:30 +0100

    Function

--- function.php
+++ function.php
@@ -1,6 +1,30 @@
 <?php include('config/connect.php'); 
 
         global $conn;
+        $stmt = $mysqli->prepare("SELECT email FROM submissions");
+        $stmt->execute();
+        $array = [];
+        foreach ($stmt->get_result() as $row)
+        {
+            $queryURL = "SELECT points FROM submissions WHERE user = $row ";
+            $resultURL = mysqli_query($conn, $queryURL);
+            $countURL = mysqli_num_rows($resultURL);
+            $total = 0;
+            if ($countURL > 0) {
+                while($row = $resultURL->fetch_assoc()) {
+                $total += $row['points'];
+            }
+                return $total;
+        }
+            else{
+                return $total;
+        }
+        /*$email = $_SESSION['login_user'];
+        $total = total_score($email);
+        $sql = "UPDATE `user` SET score = $total WHERE email = $email";
+        $result = mysqli_query($conn, $sql);
+        $array[] = $row['column'];
+        }
         $queryURL = "SELECT points FROM submissions WHERE user = $email ";
         $resultURL = mysqli_query($conn, $queryURL);
         $countURL = mysqli_num_rows($resultURL);
@@ -13,12 +37,11 @@
         }
         else{
             return $total;
-        }
-        $email = $_SESSION['login_user'];
-        $total = total_score($email);
-        $sql = "UPDATE `user` SET score = $total WHERE email = $email";
+        }*/
+        $total = total_score($row);
+        $sql = "UPDATE `user` SET score = $total WHERE email = $row";
         $result = mysqli_query($conn, $sql);
-    //do the rest, i'm blank
+    //do the rest, i'm blank 
 ?>
 
 
