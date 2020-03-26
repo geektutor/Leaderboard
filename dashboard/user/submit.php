75a8f16161d02e34f6e4cf9ac5d06bf6b591d091
geektutor <sodiq.akinjobi@gmail.com>
2020-03-26 17:06:31 +0100

    Add whatsapp support

--- dashboard/user/submit.php
+++ dashboard/user/submit.php
@@ -62,7 +62,7 @@ if(isset( $_SESSION['login_user'])){
                             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-paper-plane"></i></div>
                             </a>
                             <a class="nav-link" href="https://30daysofcode.xyz/whatsapp">
-                                <div class="sb-nav-link-icon"><i class="fab fa-whatsapp"></i></div>
+                                <div class="sb-nav-link-icon"><i class="fas fa-phone"></i></div>
                             Support Group
                             </a>
                             <a class="nav-link" href="submit.php"
