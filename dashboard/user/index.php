2815440271da062b001d45543bf273b1dce43ec8
geektutor <sodiq.akinjobi@gmail.com>
2020-03-26 05:51:49 +0100

    Added Updates

--- dashboard/user/index.php
+++ dashboard/user/index.php
@@ -194,10 +194,9 @@ if(isset( $_SESSION['login_user'])){
         </div>
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="../js/scripts.js"></script>
+        <script src="../dist/scripts.js"></script>
         <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-        <script src="../assets/demo/datatables-demo.js"></script>
     </body>
 </html>
 <?php
