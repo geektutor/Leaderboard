6d04e785293ebef91f0425de450a1f7f07de087a
geektutor <sodiq.akinjobi@gmail.com>
2020-03-26 21:46:34 +0100

    Fixed Leaderboard

--- function.php
+++ function.php
@@ -1,27 +1,34 @@
-<?php include('config/connect.php'); 
-
+<?php
+include './config/connect.php';
+//i don't know what i am writing tho, i hope it works sha
+    function total_score($email){   
         global $conn;
-        $query = "SELECT email FROM submissions";
-        $count = mysqli_query($conn, $query);
-        $countRows = mysqli_num_rows($count);
-        var_dump($countRows);
-        if ($countRows > 0) {
-            $queryURL = "SELECT points FROM submissions WHERE user = $row ";
-            $resultURL = mysqli_query($conn, $queryURL);
-            $countURL = mysqli_num_rows($resultURL);
-            $total = 0;
-            if ($countURL > 0) {
-                while($row = $resultURL->fetch_assoc()) {
+        $queryURL = "SELECT `points` FROM submissions WHERE `user` = '$email' ";
+        $resultURL = mysqli_query($conn, $queryURL);
+        $countURL = mysqli_num_rows($resultURL);
+        $total = 0;
+        if ($countURL > 0) {
+            while($row = mysqli_fetch_assoc($resultURL)) {
                 $total += $row['points'];
             }
-                return $total;
-                $sql = "UPDATE `user` SET score = $total WHERE email = $row";
-                $result = mysqli_query($conn, $sql);
-            }
-            else{
-                return $total;
-            }   
+            return $total;
+        }else{
+            return $total;
         }
-            
-    //do the rest, i'm blank 
+    }
+    //$email = $_SESSION['login_user'];
+    $email = "kruse@gmail.com";
+    $total = total_score($email);
+    $sql = "UPDATE user SET `score` = '$total' WHERE email = '$email'";
+    if(mysqli_query($conn, $sql)){
+        echo 'successful';
+    }else {
+        echo 'error';
+    }
+
+    //do the rest, i'm blank
 ?>
+
+
+
+
