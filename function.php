4cafeb7719fdd8ab4cc5524e56c91b4bda84f96c
geektutor <sodiq.akinjobi@gmail.com>
2020-03-26 19:28:52 +0100

    Function

--- function.php
+++ function.php
@@ -15,35 +15,14 @@
                 $total += $row['points'];
             }
                 return $total;
-        }
+            }
             else{
                 return $total;
-        }
-        /*$email = $_SESSION['login_user'];
-        $total = total_score($email);
-        $sql = "UPDATE `user` SET score = $total WHERE email = $email";
-        $result = mysqli_query($conn, $sql);
-        $array[] = $row['column'];
-        }
-        $queryURL = "SELECT points FROM submissions WHERE user = $email ";
-        $resultURL = mysqli_query($conn, $queryURL);
-        $countURL = mysqli_num_rows($resultURL);
-        $total = 0;
-        if ($countURL > 0) {
-            while($row = $resultURL->fetch_assoc()) {
-                $total += $row['points'];
             }
-            return $total;
+            $array[] = $row['column'];
         }
-        else{
-            return $total;
-        }*/
         $total = total_score($row);
         $sql = "UPDATE `user` SET score = $total WHERE email = $row";
         $result = mysqli_query($conn, $sql);
     //do the rest, i'm blank 
 ?>
-
-
-
-
