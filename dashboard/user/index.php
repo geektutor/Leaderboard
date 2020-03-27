cf4a691d66190065c633f4759e4f3415d12892d1
Adebayo Adekoya <adekoya.adebayojubril@gmail.com>
2020-03-27 09:23:26 +0100

    version 1.0.0

--- dashboard/user/index.php
+++ dashboard/user/index.php
@@ -138,27 +138,4 @@ if(isset( $_SESSION['login_user'])){
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; 30DayOfCode 2020</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
-        </div>
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="../js/scripts.js"></script>
-        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-        <script src="../assets/demo/datatables-demo.js"></script>
-    </body>
-</html>
-<?php
-}else{
-    header("location:../../login.php"); 
-}
-?>
+                            <div class="te
