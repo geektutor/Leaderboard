77afce82d560ae80a69816aa372fee165e44dbd4
geektutor <sodiq.akinjobi@gmail.com>
2020-03-30 08:34:08 +0100

    Fix Scripts

--- dashboard/user/task.php
+++ dashboard/user/task.php
@@ -25,11 +25,11 @@ require('../../config/connect.php');
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="viewport" content="width=device-width, inistial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Dashboard - 30 Days Of Code</title>
-    <link href="../dist/css/styles.css" rel="stylesheet" />
+    <link href="../../dist/css/styles.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet"
         crossorigin="anonymous" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js"
@@ -155,7 +155,7 @@ require('../../config/connect.php');
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"
         crossorigin="anonymous"></script>
-    <script src="../dist/js/scripts.js"></script>
+    <script src="../../dist/js/scripts"></script>
     <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
 </body>
