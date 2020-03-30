7a4c22d7630c00bb8c53d72653f21a86fceffa15
geektutor <sodiq.akinjobi@gmail.com>
2020-03-30 17:07:47 +0100

    Fixed Feedback

--- dashboard/user/task.php
+++ dashboard/user/task.php
@@ -34,8 +34,6 @@ require('../../config/connect.php');
     <meta name="author" content="" />
     <title>Dashboard - 30 Days Of Code</title>
     <link href="../dist/css/styles.css" rel="stylesheet" />
-    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet"
-        crossorigin="anonymous" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js"
         crossorigin="anonymous"></script>
 </head>
@@ -163,9 +161,6 @@ require('../../config/connect.php');
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"
         crossorigin="anonymous"></script>
     <script src="../dist/js/scripts.js"></script>
-    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-    <script src="../assets/demo/datatables-demo.js"></script>
 </body>
 
 </html>
