5e6f449e3499fcb28f385ede3933731ed9de2549
geektutor <sodiq.akinjobi@gmail.com>
2020-03-31 03:46:08 +0100

    Fixed Task

--- dashboard/user/task.php
+++ dashboard/user/task.php
@@ -44,17 +44,17 @@ require('../../config/connect.php');
         <a class="navbar-brand" href="index.php">30DaysOfCode.xyz</a><button
             class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i
                 class="fas fa-bars"></i></button>
-        <!-- Navbar Search
+        <!-- Navbar Search-->
         <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
             <div class="input-group">
-                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search"
-                    aria-describedby="basic-addon2" />
+                <!--<input class="form-control" type="text" placeholder="Search for..." aria-label="Search"
+                    aria-describedby="basic-addon2" />-->
                 <div class="input-group-append">
-                    <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
+                    <!--<button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>-->
                 </div>
             </div>
-        </form>-->
-        <!-- Navbar
+        </form>
+        <!-- Navbar-->
         <ul class="navbar-nav ml-auto ml-md-0">
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown"
@@ -65,7 +65,7 @@ require('../../config/connect.php');
                     <a class="dropdown-item" href="login.html">Logout</a>
                 </div>
             </li>
-        </ul>-->
+        </ul>
     </nav>
     <div id="layoutSidenav">
         <div id="layoutSidenav_nav">
