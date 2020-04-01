246a5c3d35d70c766ca91553d76f8385cf9f3def
geektutor <sodiq.akinjobi@gmail.com>
2020-04-01 13:15:52 +0100

    Fix Submit

--- dashboard/user/submit.php
+++ dashboard/user/submit.php
@@ -199,6 +199,7 @@ if(isset( $_SESSION['login_user'])){
                                       <option value="Day 5">Day 5</option>
                                       <option value="Day 6">Day 6</option>
                                       <option value="Day 7">Day 7</option>
+                                      <option value="Day 8">Day 8</option>
                                     </select>
                                     </div>
                                     <div class="form-group">
