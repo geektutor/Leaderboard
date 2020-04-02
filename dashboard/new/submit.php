2d31f9c6e25bf3cf0bdad4b39211e35d0af8b6b2
geektutor <sodiq.akinjobi@gmail.com>
2020-04-02 14:14:28 +0100

    New Experience

--- dashboard/new/submit.php
+++ dashboard/new/submit.php
@@ -210,7 +210,8 @@ if(isset( $_SESSION['login_user'])){
  </div>
  <script src="./assets/js/app.js"></script>
 </body>
-</html><?php
+</html>
+<?php
 }else{
     header("location:../../login.php"); 
 }
