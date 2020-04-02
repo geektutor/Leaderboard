d521976115cde83a371af55515798415a6b0dc91
geektutor <sodiq.akinjobi@gmail.com>
2020-04-02 09:59:39 +0100

    Make changes to Sign Up

--- 30days/login.php
+++ 30days/login.php
@@ -1,24 +1,24 @@
-<?php include('config/connect.php'); ?>
+<?php include('../config/connect.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="signup.css">
-    <title>30DaysOfCode  - Login</title>
+    <title>Login - 30DaysOfCode</title>
+    <link rel="stylesheet" rel="shortcut icon" type="image/png" href="img/fastreboot_arrow_6026.ico">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="rajstyle.css">
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
 </head>
 <body>
-    <div class="contact-us">
-    <?php
+<?php
         $ref = substr(@$_SERVER['HTTP_REFERER'],strlen(@$_SERVER['HTTP_REFERER']) - 10, 10);
         $resetPassword = substr(@$_SERVER['HTTP_REFERER'],strlen(@$_SERVER['HTTP_REFERER']) - 15, 15);
         if (@$_GET['message'] == 'success' && $ref == 'signup.php') {
-            echo "<div class='msg alert-success alert-dismissable'>Registration Successful </div>";
+            echo "<div class='group'>Registration Successful </div>";
         }
         if (@$_GET['message'] == 'success' && $resetPassword == 'newpassword.php') {
-            echo "<div class='msg alert-success alert-dismissable'>Password reset Successful. kindly log into your account.</div>";
+            echo "<div class='group'>Password reset Successful. kindly log into your account.</div>";
         }
         ?>
         <?php
@@ -74,17 +74,25 @@
         
         ?>
         <?php if($error !== ''){ ?>
-        <div class="alert alert-primary alert-dismissable">
+        <div class="group">
             <?= $error?>
         </div>
         <?php }?>
-        <form method="POST">
-          <input name="email" placeholder="email" required="" type="email" value="" />
-          <input name="password" placeholder="password" type="password" value="" required/>
-          <button type="submit" name="submit" value="submit">Login</button>
-        </form><br>
-        <p>Not already a user ? <a href="signup.php"> Signup here </a></p>
-        <a href="./forgotpassword.php">Forgot Password ?</a>
-      </div>
+
+    <form class="login">
+        <i class="fa fa-universal-access"></i>
+        <h2>30 Days Of Code</h2>
+        <div class="group">
+            <input type="email" name="email" placeholder="Email" required><i class="fa fa-user"></i>
+        </div>
+        <div class="group">
+            <input type="password" name="password" placeholder="Password" required><i class="fa fa-lock"></i>
+        </div>
+        
+        <button type+"submit" name="submit" value="submit"><i class="fa fa-send"></i> Login</button>
+        <p >Forgot <a href="forget.php">Password</a>?</p>
+        <p >Don't have an account ? <a href="signup.php">Signup</a></p>
+    </form>
+    
 </body>
 </html>
