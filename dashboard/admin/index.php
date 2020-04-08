45d2a8e7b7a9a5eaa4fbd963727345a025599574
geektutor <sodiq.akinjobi@gmail.com>
2020-04-08 03:06:21 +0100

    Made changes

--- dashboard/admin/index.php
+++ dashboard/admin/index.php
@@ -113,17 +113,8 @@ if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
                                     $sql = "SELECT s.*, u.university FROM submissions AS s";
 
                                     if ($university == ''){
-                                        $sql = "SELECT s.*, u.university FROM submissions AS s
-
-                                            LEFT JOIN user AS u ON s.user = u.email  
+                                        $sql = "SELECT s.*, u.university FROM submissions AS s LEFT JOIN user AS u ON s.user = u.email  
                                             WHERE s.track = '$track' AND s.points = 0 AND u.university = '$university'";
-                                    $result = mysqli_query($conn, $sql);
-                                    $count = mysqli_num_rows($result);
-                                    }
-                                    else if ($university == 'admin'){
-                                        $sql = "SELECT s.*, u.university FROM submissions AS s
-                                            LEFT JOIN user AS u ON s.user = u.email  
-                                             AND u.university = '$university' ORDER BY s.points";
                                     $result = mysqli_query($conn, $sql);
                                     $count = mysqli_num_rows($result);
                                     }
