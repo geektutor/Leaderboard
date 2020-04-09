2a026f13ce5e8d19cec506d83aac415ee0162fb5
Sodiq Akinjobi <sodiq.akinjobi@gmail.com>
2020-04-09 12:25:06 +0100

    Update index.php

--- dashboard/admin/index.php
+++ dashboard/admin/index.php
@@ -206,7 +206,7 @@ if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
         </div>
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="../js/scripts.js"></script>
+        <script src="../dist/js/scripts.js"></script>
         <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
         <script src="../assets/demo/datatables-demo.js"></script>
