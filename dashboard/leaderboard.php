92eadfa7f425ea73dea5dbcf1d604c30f2e33d26
geektutor <sodiq.akinjobi@gmail.com>
2020-04-09 20:58:32 +0100

    Added Favicon

--- dashboard/leaderboard.php
+++ dashboard/leaderboard.php
@@ -69,6 +69,7 @@
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
       <link rel="stylesheet" href="./index.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <link rel="shortcut icon" href="https://30daysofcode.xyz/favicon.png" type="image/x-icon">
     </head>
     <body>
       <div class="filter">
