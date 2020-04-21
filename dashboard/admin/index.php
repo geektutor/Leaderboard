475fef77f72013395940a2e8be15725b7667b637
Sodiq Akinjobi <sodiq.akinjobi@gmail.com>
2020-04-21 11:03:11 +0100

    Update index.php

--- dashboard/admin/index.php
+++ dashboard/admin/index.php
@@ -124,7 +124,7 @@ if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
                                         $sql = "SELECT s.*, u.university 
                                             FROM submissions AS s 
                                             LEFT JOIN user AS u ON s.user = u.email  
-                                            WHERE s.track = '$track' AND s.points = 0 AND u.university = '$university' AND s.task_day < 'Day 028'";
+                                            WHERE s.track = '$track' AND s.points = 0 AND u.university = '$university' AND s.task_day < 'Day 028' ORDER BY s.task_day";
                                     }else{
                                         $sql = "SELECT s.*, u.university FROM submissions AS s
                                             LEFT JOIN user AS u ON s.user = u.email  
