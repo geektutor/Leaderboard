5d5cc25d5d970cfd14b73b2e96ebec5743ca34bc
Emmanuerl <emekaemmanuel045@gmail.com>
2020-04-23 13:30:52 +0100

    certificate feature

--- dashboard/user/certificate.php
+++ dashboard/user/certificate.php
@@ -11,6 +11,7 @@ if (isset($_SESSION['login_user'])) {
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
  <link rel="stylesheet" href="./assets/css/style.css">
  <link rel="stylesheet" href="./assets/css/submissions.css">
  <link rel="stylesheet" href="./assets/css/responsive.css">
