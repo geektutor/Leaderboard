9d214ddf7f105ad710e2d3343913feec059a27c1
geektutor <sodiq.akinjobi@gmail.com>
2020-04-25 16:06:25 +0100

    Feedback

---
+++ dashboard/user/feedback.php
@@ -0,0 +1,176 @@
+<?php
+require('../../config/connect.php');
+require('../../config/session.php');
+if(isset( $_SESSION['login_user'])){
+    $tt = $_SESSION['login_user'];
+    $sql = "SELECT track FROM user WHERE email = '$tt'";
+    $result = mysqli_query($conn, $sql);
+    $row =mysqli_fetch_assoc($result);
+    $track = $row['track'];
+    // $university = $row['university'];
+
+?>
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <link rel="stylesheet" href="./assets/css/style.css">
+ <link rel="stylesheet" href="./assets/css/submit.css">
+ <link rel="stylesheet" href="./assets/css/responsive.css">
+ <title>Feedback - 30 Days Of Code</title>
+</head>
+<body class="flx col">
+ <header class="flx row">
+  <span>#30DaysOfCode</span>
+  <div class="techSymb flx row">
+   <img src="./assets/img/htm.png">
+   <img src="./assets/img/crly.png">
+   <img src="./assets/img/prts.png">
+   <img src="./assets/img/dsg.png">
+  </div>
+  <div class="profile flx col">
+    <img src="./assets/img/profile.png">
+    <ul class="options">
+      <li id="logout"><a href="../../logout.php">Logout</a></li>
+    </ul>
+  </div>
+ </header>
+ <div class="pageWrapper flx row">
+  <nav class="flx col closed" id="navPane">
+    <div class="hamBWrapper">
+      <div id="hamB" class="closed">   
+        <div class="a"></div> 
+        <div class="b"></div>
+        <div class="c"></div>
+      </div>
+    </div>
+     <div class="flx col content">
+      <?php
+      global $conn;
+      $user_nickname = '';
+      $user_score = '';
+      $user_track = '';
+      $email = $_SESSION['login_user'];
+      $sql = "SELECT * FROM user WHERE email='$email' ";
+      $result = mysqli_query($conn,$sql);
+      while($row = mysqli_fetch_assoc($result)) {
+          $user_nickname = $row['nickname'];
+          $user_score = $row['score'];
+          $user_track = $row['track'];
+          echo '<div class="avatar"><img style=\'width:120px;height:120px;\' src=\'https://robohash.org/'.$user_nickname.$user_track.'\'/></div>';
+          echo '<span id="username">'.$user_nickname.'</span>';
+          // echo '<span id="username">'.$user_score.'&nbsp; points</div></center>';
+      }
+      ?>
+      <div class="scoresContainer flx row">
+        <div class="scoreCard flx col">
+         <div class="wLayer"></div>
+         <span class="title">Total points:</span>
+         <span id="points"><?php echo '<center><div>'.$user_score.'&nbsp; points</div></center>';?></span>
+        </div>
+        <div class="scoreCard flx col">
+         <div class="wLayer"></div>
+         <span class="title">Total rank:</span>
+           <?php
+            global $conn;
+            $ranking_sql = "SELECT * FROM user WHERE `isAdmin` = '0' ORDER BY `score` DESC";
+            $ranking_result = mysqli_query($conn,$ranking_sql);
+            if ($ranking_result) {
+                $rank = 1;
+                while ($row = mysqli_fetch_assoc($ranking_result)) {
+                    if($row['email'] == $email){
+                        echo '<span id="rank">'.$rank.'</span>';
+                    }else {
+                        $rank++;
+                    }
+                }
+                
+            }else {
+                echo "error fetching from database";
+            }
+            ?>
+        </div>
+      </div>
+      <ul class="linksContainer">
+        <li class="flx row">
+         <img src="./assets/img/submsn.png">
+         <a href="index.php">Submissions</a>
+        </li>
+        <li class="flx row">
+         <img src="./assets/img/allTsk.png">
+         <a href="view.php">All tasks</a>
+        </li>
+        <li class="flx row active">
+         <img src="./assets/img/add.png">
+         <a href="submit.php">Submit task</a>
+        </li>
+        <li class="flx row">
+         <img src="./assets/img/lead.png">
+         <a href="https://30daysofcode.xyz/dashboard/leaderboard.php">Leaderboard</a>
+        </li>
+        <li class="flx row">
+         <img src="./assets/img/tweet.png">
+         <a href=" https://twitter.com/intent/tweet?url=https%3A%2F%2F30daysofcodes.xyz&via=codon&text=Hello%2C%20I%20just%20finished%20my%20task%20for%20....&hashtags=30DaysOfCode%2C%20ECX">Tweet</a>
+         <img class="external" style="float: right;" src="./assets/img/external.png" alt="">
+        </li>
+        <li class="flx row">
+         <img src="./assets/img/wa.png">
+         <a href="https://30daysofcode.xyz/whatsapp">Support group</a>
+         <img class="external" src="./assets/img/external.png" alt="">
+        </li>
+       </ul>
+       <span id="email"><?=$_SESSION['login_user'];?></span>
+     </div>   
+   </nav>
+   <div class="mainWrapper flx col" id="mainWrp">
+    <main>
+      <div class="flx row"><h1>Submit Feedback</h1></div>
+      <div class="mainCard">
+      <?php
+      $error = "";
+        if(isset($_POST['submit'])){
+        $user =  mysqli_real_escape_string($conn, $_SESSION['login_user']);
+        $feedback =  mysqli_real_escape_string($conn, $_POST['feedback']);
+        $sql = "INSERT INTO feedback(user, feedback) VALUES('$user','$feedback')";
+        if($conn->query($sql)){
+            $error = "Submitted Successfully";
+            $submit = 1;
+        }
+        else{
+            die('could not enter data: '. $conn->error);
+        }
+        }
+      ?>
+      <?php if($error !== ''){ ?>
+      <div class="notice">
+          <?php 
+              echo $error;
+              if($submit == 1){
+          ?>
+          <p>Share on twitter:</p>
+          <a href="https://twitter.com/intent/tweet?url=http%3A%2F%2F30daysofcode.xyz&via=ecxunilag&text=I%20just%20completed%20&hashtags=30DaysOfCode%20">
+          <button class="flx row"> <img src="./assets/img/tweet2.png"> Tweet</button>
+          </a>
+          <?php }?>
+          </div>
+              <?php }?>
+       <form method="POST">
+          <div class="field flx col">
+            <label for="feedback">Anonymous Feedback?</label>
+            <textarea name="feedback" type="text" placeholder="Any comments?" rows="8"></textarea>
+          </div>
+          <button id="submitTask" type="submit" name="submit">Submit Feedback</button>
+        </form> 
+      </div >
+     </main>
+     <footer class="flx row"><span class="copyw">Copyright &copy; 30DaysOfCode 2020</span> <div><a href="">Privacy Policy</a><a href="">Terms & Conditions</a></div></footer> 
+   </div>
+ </div>
+ <script src="./assets/js/app.js"></script>
+</body>
+</html>
+<?php
+}else{
+  header("location:../../login.php");
+}
+?>
