cbd16d039551fcd0664bdbb6887c0ef633269f61
AdavizeTheFirst <42492202+AdavizeTheFirst@users.noreply.github.com>
2020-04-25 13:45:10 +0100

    changed src here

--- dashboard/user/view.php
+++ dashboard/user/view.php
@@ -140,7 +140,7 @@ if(isset($_POST['check_task'])){
          <a href="submit.php">Submit task</a>
         </li>
         <li class="flx row">
-         <img src="./assets/img/add.png">
+         <img src="./assets/img/cert.png">
          <a href="certification.php">Certificate</a>
         </li>
         <li class="flx row">
