3a069e469f531635a5a6b261baa9b0a29c027f0c
Sodiq Akinjobi <sodiq.akinjobi@gmail.com>
2020-04-26 16:08:27 +0100

    Update certification.php

--- dashboard/user/certification.php
+++ dashboard/user/certification.php
@@ -156,7 +156,7 @@ if(isset( $_SESSION['login_user'])){
             $sql = "SELECT DISTINCT `sub_date` FROM submissions WHERE `user` = {user}";
             $result = mysqli_query($conn,$sql);
     if ($result) {
-                if(mysqli_num_rows($result) <= 2){ ?>
+                if(mysqli_num_rows($result) <= 15){ ?>
                     <p>You're not eligible to be certified</p>
                 <?php }else { ?>
                     <p style='font-size: 1em; margin-top: 8px; line-height: 110%; color: #646464;'>
