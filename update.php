c692be410e9149e6402334ff936163e7c50fe3fc
geektutor <sodiq.akinjobi@gmail.com>
2020-05-01 16:02:18 +0100

    Updated Update

--- update.php
+++ update.php
@@ -31,7 +31,7 @@
         WHERE `email` = '$email' ";
        $result = mysqli_query($conn,$sql);
         if($result){
-        header("location:sign_in.php?message=update");
+        header('location:sign_in.php?message=update');
         }
         else{
             $error = "Email incorrect";
