e6f5ebe8bf3cb82a6f826736309cf94862e0ff57
Sodiq Akinjobi <sodiq.akinjobi@gmail.com>
2020-05-01 13:13:24 +0100

    Update submit.php

--- user/submit.php
+++ user/submit.php
@@ -195,7 +195,7 @@ if(isset( $_SESSION['login_user'])){
               if($submit == 1){
           ?>
           <p>Share on twitter:</p>
-          <a href="https://twitter.com/intent/tweet?url=https%3A%2F%2F30daysofcode.xyz%2F&via=ecxunilag&text=<?php echo $task_day;?>%20of%2030%3A%20Check%20out%20my%20solution%20at%3A%20<?php echo $url;?>&hashtags=30DaysOfCode%2C%2030DaysOfDesign%2C%20ecxunilag">
+          <a href="https://twitter.com/intent/tweet?url=https%3A%2F%2F30daysofcode.xyz%2F&via=try30DaysOfCode&text=<?php echo $task_day;?>%20of%2030%3A%20Check%20out%20my%20solution%20at%3A%20<?php echo $url;?>&hashtags=30DaysOfCode%2C%2030DaysOfDesign">
           <button class="flx row"> <img src="./assets/img/tweet2.png"> Tweet</button>
           </a>
           <?php }?>
