a225624e33ee9e87847a5882fa3e4f314904c5bc
geektutor <sodiq.akinjobi@gmail.com>
2020-05-01 20:09:51 +0100

    Updated Leaderboard

--- leaderboard/index.php
+++ leaderboard/index.php
@@ -1,67 +1,65 @@
 <?php
-  $userRanking = array();
-  function getUSerRankings($fetched_array)
-  {
-    include "../config/connect.php";
+include "../config/connect.php";
+$userRanking = [];
+//user class for each fetched user
+class User
+{
+    public $nickname;
+    public $track;
+    public $level;
+    public $score;
 
-    //user class for each fetched user
-    class User
-    {
-      public $nickname;
-      public $track;
-      public $score;
-
-      function __construct($nickname,$track,$score){
-        $this->nickname = $nickname;
-        $this->track = $track;
-        $this->score = $score;
-      }
+    function __construct($nickname,$track,$level,$score){
+    $this->nickname = $nickname;
+    $this->track = $track;
+    $this->level = $level;
+    $this->score = $score;
     }
+}
 
-    if (isset($_GET['filter']) && !empty($_GET['filter'])) {
-      //preparing the query parameter
-      $filter = $_GET['filter'];
-      $filter = explode('+',$filter);
-      function myfunction($v1,$v2)
-      {
-      return $v1 . " " . $v2;
-      }
-      $filter = array_reduce($filter,"myfunction");
-      $filter = trim($filter);
+//categories for filter 
+if (isset($_GET['track']) and isset($_GET['level']) and $_GET['level'] !== 'general') {
+    $track = $_GET['track'];
+    $level = $_GET['level'];    
+    $sql = "SELECT * FROM leaderboard WHERE `track`='$track' AND `level` = '$level' ORDER BY `score` DESC LIMIT 20";
+}
+elseif (isset($_GET['track']) and $_GET['track'] == 'general' and isset($_GET['level'])) {
+    $level = $_GET['level'];    
+    $sql = "SELECT * FROM leaderboard WHERE `level` = '$level' ORDER BY `score` DESC LIMIT 20";
 
+}
+elseif (isset($_GET['track']) and $_GET['track'] == 'general' and !isset($_GET['level'])) {    
+    $sql = "SELECT * FROM leaderboard ORDER BY `score` DESC LIMIT 20";
 
-      switch ($filter) {
-        case 'General':
-          $val = '';
-          $sql = "SELECT * FROM user WHERE `isAdmin` = 0 And `university`='$val' ORDER BY `score` DESC LIMIT 20";
-          break;
-
-        default:
-          $sql = "SELECT * FROM user WHERE `isAdmin` = 0 AND `university` ='$filter' ORDER BY `score` DESC LIMIT 20";
-          break;
-      }
-    }else{
-      $val = '';
-      $sql = "SELECT * FROM user WHERE `isAdmin` = 0 And `university` = '$val' ORDER BY `score` DESC LIMIT 20";
+}
+elseif (isset($_GET['track']) and !isset($_GET['level'])) {
+    $track = $_GET['track'];    
+    $sql = "SELECT * FROM leaderboard WHERE `track`='$track' ORDER BY `score` DESC LIMIT 20";
+}
+elseif (!isset($_GET['track']) and isset($_GET['level'])) {
+    $level = $_GET['level'];    
+    $sql = "SELECT * FROM leaderboard WHERE `level` = '$level' ORDER BY score DESC LIMIT 20";
+}
+else {    
+    $sql = "SELECT * FROM leaderboard ORDER BY `score` DESC LIMIT 20";
+}
+$result = mysqli_query($conn,$sql);
+if ($result) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $nickname = $row['nickname'];
+        $track = $row['track'];
+        $level = $row['level'];
+        $score = $row['score'];
+        $user = new User($nickname,$track,$level,$score);
+        array_push($userRanking,$user);
     }
-    $result = mysqli_query($conn,$sql);
-    if ($result) {
-      echo "worked";
-    }
-    if (mysqli_num_rows($result) > 0) {
-      while ($row = mysqli_fetch_assoc($result)) {
-          $nickname = $row['nickname'];
-          $track = $row['track'];
-          $score = $row['score'];
-          $user = new User($nickname,$track,$score);
-          array_push($fetched_array,$user);
-      }
-    }
-    $res =json_encode($fetched_array);
+}else{
+    die('error fetching rankings please try again later');
+}
+$res =json_encode($userRanking);
     $file = fopen('results.json','w') or die('error creating file');
     fwrite($file,$res);
-  }
-  getUSerRankings($userRanking);
+
  ?>
  <!DOCTYPE html>
 <html>
@@ -72,69 +70,72 @@
     </head>
     <body>
       <div class="filter">
-        <form id="filterform">
-          <select name="" id="filter" class="form-control">
+        <form id="filterform" action="leaderboard1.php" method="GET">
+          <select name="level" id="filter" class="form-control">
+           <option value="beginner">Beginner</option>
+           <option value="intermediate">Intermediate</option>
+          </select>
+          <select name="track" id="filter" class="form-control">
             <?php
             include "../config/connect.php";
-            $sql = "SELECT DISTINCT `university` FROM user";
+            $sql = "SELECT DISTINCT `track` FROM user";
             $result = mysqli_query($conn,$sql);
             if ($result && mysqli_num_rows($result) > 0) {
               while ($row = mysqli_fetch_assoc($result)) {
-                $row['university'] == ''?$row['university'] = 'General' : true;
-                echo "<option value='".$row['university']."' id='".$row['university']."'>".$row['university']."</option>";
+                $row['track'] == ''?$row['track'] = 'general' : true;
+                echo "<option value='".$row['track']."' id='".$row['track']."'>".$row['track']."</option>";
               }
             }
              ?>
           </select>
           <button type="submit" class="btn btn-warning">Filter</button>          
-          <a href="track.php" class="btn btn-warning">Filter By Track</a>
         </form>
       </div>
       <div class="center">
         <div class="top3">
           <div class="two item">
             <div class="pos">
-              2
+              
             </div>
-            <div class="pic" style="background-image: url(&#39;https://randomuser.me/api/portraits/men/44.jpg&#39;)"></div>
+            <div class="pic" style="background-image: url()"></div>
             <div class="name">
-              Ifihan Olusheye
+              
             </div>
-            <div class="track">empty</div>
+            <div class="track"></div>
             <div class="score">
-              30
+              
             </div>
           </div>
           <div class="one item">
             <div class="pos">
-              1
+              
             </div>
-            <div class="pic" style="background-image: url(&#39;https://randomuser.me/api/portraits/men/31.jpg&#39;)"></div>
+            <div class="pic" style="background-image: url()"></div>
             <div class="name">
-              Geektutor
+             
             </div>
             <div class="track"></div>
             <div class="score">
-              10
+              
             </div>
           </div>
           <div class="three item">
             <div class="pos">
-              3
+              
             </div>
-            <div class="pic" style="background-image: url(&#39;https://randomuser.me/api/portraits/women/91.jpg&#39;)"></div>
+            <div class="pic" style="background-image: url()"></div>
             <div class="name">
-              Akin Aguda
+           
             </div>
             <div class="track"></div>
             <div class="score">
-              50
+              
             </div>
           </div>
         </div>
           <div class="list others">
           </div>
         </div>
-      <script src="leaderboard.js"></script>
+      <script src="leaderboard1.js"></script>
     </body>
 </html>
