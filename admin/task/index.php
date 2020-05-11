62e3f16a1351ccb9066e1f5e55265f138a57ec1c
geektutor <sodiq.akinjobi@gmail.com>
2020-05-12 00:58:14 +0100

    Dix Taks

--- admin/task/index.php
+++ admin/task/index.php
@@ -1,10 +1,12 @@
 <?php
-require('../../config/connect.php');
-require('../../config/session.php');
-
+require('../config/connect.php');
+require('../config/session.php');
 if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
-  $track = $_GET['track'];
-  $level = $_GET['level'];
+    if (isset($_POST['submit'])) {
+        $track = $_POST['track'];
+        $level = $_POST['level'];
+        header("location: task.php?track=$track&level=$level");
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,23 +14,23 @@ if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <meta http-equiv="X-UA-Compatible" content="ie=edge">
- <link rel="stylesheet" href="../../assets/css/style.css">
- <link rel="stylesheet" href="../../assets/css/submissions.css">
- <link rel="stylesheet" href="../../assets/css/responsive.css">
- <link rel="shortcut icon" href="../../assets/img/favicon.png" type="image/x-icon">
+ <link rel="stylesheet" href="../assets/css/style.css">
+ <link rel="stylesheet" href="../assets/css/submissions.css">
+ <link rel="stylesheet" href="../assets/css/responsive.css">
+ <link rel="shortcut icon" href="../assets/img/favicon.png" type="image/x-icon">
  <title>Dashboard - 30 Days Of Code</title>
 </head>
 <body class="flx col">
  <header class="flx row">
   <span>#30DaysOfCode</span>
   <div class="techSymb flx row">
-   <img src="../../assets/img/htm.png">
-   <img src="../../assets/img/crly.png">
-   <img src="../../assets/img/prts.png">
-   <img src="../../assets/img/dsg.png">
+   <img src="../assets/img/htm.png">
+   <img src="../assets/img/crly.png">
+   <img src="../assets/img/prts.png">
+   <img src="../assets/img/dsg.png">
   </div>
   <div class="profile flx col">
-    <img src="../../assets/img/profile.png">
+    <img src="../assets/img/profile.png">
     <ul class="options">
       <li id="logout"><a href="../../logout.php">Logout</a></li>
     </ul>
@@ -51,15 +53,15 @@ if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
         </li>
         <li class="flx row">
          <img src="../../assets/img/add.png">
-         <a href='/task/addnewtask.php'>Add New Tasks</a>
+         <a href='addnewtask.php'>Add New Tasks</a>
         </li>
         <li class="flx row">
          <img src="../../assets/img/allTsk.png">
-         <a href="/task">View Tasks</a>
+         <a href="super.php">All Tasks</a>
         </li>
         <li class="flx row">
          <img src="../../assets/img/add.png">
-         <a href="../superadmin.php">Superadmin</a>
+         <a href="../superadmin.php">Super Admin</a>
         </li>
         <li class="flx row">
          <img src="../../assets/img/lead.png">
@@ -68,12 +70,12 @@ if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
         <li class="flx row">
          <img src="../../assets/img/tweet.png">
          <a href=" https://twitter.com/intent/tweet?url=https%3A%2F%2F30daysofcodes.xyz&via=codon&text=Hello%2C%20I%20just%20finished%20my%20task%20for%20....&hashtags=30DaysOfCode%2C%20ECX">Tweet</a>
-         <img class="external" style="float: right;" src="../../assets/img/external.png" alt="">
+         <img class="external" style="float: right;" src="../assets/img/external.png" alt="">
         </li>
         <li class="flx row">
          <img src="../../assets/img/wa.png">
          <a href="https://30daysofcode.xyz/whatsapp">Support group</a>
-         <img class="external" src="../../assets/img/external.png" alt="">
+         <img class="external" src="../assets/img/external.png" alt="">
         </li>
        </ul>
        <span id="email"><?=$_SESSION['login_user'];?></span>
@@ -81,57 +83,37 @@ if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
    </nav>
    <div class="mainWrapper flx col" id="mainWrp">
     <main>
-    <div class="flx row"><h1>Tasks</h1> <a id="newBtn" href="addnewtask.php">Add new</a> </div>
+      <div class="flx row"><h1>Tasks</h1> </div>
       <div class="mainCard">
-      <?php
-        $current = date('Y-m-d');
-        $sql = "SELECT * FROM task WHERE `track` = '$track' AND `level` = '$level' ORDER BY task_day";
-        $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_assoc($result);
-        $count = mysqli_num_rows($result);
-        $day = $row['task_day'];
-    ?>
-       <div class="table-responsive">
-        <table class="table" style="text-align: left;">
-         <thead>
-          <tr>
-            <th scope="col">S/N</th>
-            <th scope="col">Day</th>
-            <th scope="col">Track</th>
-            <th scope="col">Level</th>
-            <th scope="col">Task</th>
-            <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-          <?php          
-          if($count > 0){
-              $j =1;
-              while($row = mysqli_fetch_assoc($result)) {
-          ?>
-          <tr>
-              <td data-label="S/N"><?php echo $j;?></td>
-              <td data-label="Day"><?= $row['task_day']; ?></td>
-              <td data-label="Track"><?= $row['track'];?></td>
-              <td data-label="Level"><?= $row['level'];?></td>
-              <td data-label="Task"><?= $row['task'];?></td>
-              <td data-label="Action"><a href="edit_task.php?id=<?=$row['id']?>">Edit</a> | <a href="delete.php?id=<?=$row['id']?>">Delete</a></td>
-          </tr>
-          <?php 
-              $j++;
-              }}else{
-                  echo `<p>No tasks yet</p>`;
-              }
-          ?>
-        </tbody>
-        </table>
-      </div>
+      <form method="POST">
+          <div class="field flx col">
+            <label for="track">Track</label>
+            <select name="track" value="">
+              <option value="backend">Backend</option>
+              <option value="frontend">Frontend</option>
+              <option value="mobile">Mobile</option>
+              <option value="python">Python</option>
+              <option value="ui">UI/UX</option>
+            </select>
+          </div>
+
+          <div class="field flx col">
+            <label for="track">Level</label>
+            <select name="level" value="">
+              <option value="Beginner">Beginner</option>
+              <option value="Intermediate">Intermediate</option>
+            </select>
+          </div>
+
+          <button id="submitTask" type="submit" name="submit">Submit</button>
+        </form> 
+     
       </div >
      </main>
      <footer class="flx row"><span class="copyw">Copyright &copy; 30DaysOfCode 2020</span> <div><a href="">Privacy Policy</a><a href="">Terms &amp; Conditions</a></div></footer>
    </div>
  </div>
- <script src="../../assets/js/app.js"></script>
+ <script src="../assets/js/app.js"></script>
 <script>
 setTimeout(() => {
     $('#success').hide(1000);
