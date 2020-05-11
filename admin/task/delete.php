f5ec8c96839438785955cc1baed2ebd8865cc6fa
geektutor <sodiq.akinjobi@gmail.com>
2020-05-11 14:13:48 +0100

    Updated User Task

--- admin/task/delete.php
+++ admin/task/delete.php
@@ -5,5 +5,6 @@ if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
     $id = $_GET['id'];
     $sql = "DELETE * FROM task WHERE id = '$id'";
     $result = mysqli_query($conn, $sql);
-    header("refresh: 2; url=./index.php"); 
+    header("refresh: 2; url=./index.php");
+}
 ?>
