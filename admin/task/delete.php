29228017ea9f532eb6471b7f7339c48ec741e6e9
geektutor <sodiq.akinjobi@gmail.com>
2020-05-11 19:32:11 +0100

    Fix dELETE

--- admin/task/delete.php
+++ admin/task/delete.php
@@ -3,7 +3,7 @@ require('../../config/connect.php');
 require('../../config/session.php');
 if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
     $id = $_GET['id'];
-    $sql = "DELETE * FROM task WHERE id = '$id'";
+    $sql = "DELETE FROM task WHERE id = '$id'";
     $result = mysqli_query($conn, $sql);
     header("refresh: 2; url=./index.php");
 }
