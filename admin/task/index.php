3ef04f32baf555c6b553c7cebb097de5c0db46ab
AdavizeTheFirst <aliyu854@gmail.com>
2020-05-15 22:03:04 +0100

    finished new styling for admin task

--- admin/task/index.php
+++ admin/task/index.php
@@ -15,7 +15,7 @@ if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <meta http-equiv="X-UA-Compatible" content="ie=edge">
  <link rel="stylesheet" href="../../assets/css/style.css">
- <link rel="stylesheet" href="../../assets/css/submissions.css">
+ <!-- <link rel="stylesheet" href="../../assets/css/submissions.css"> -->
  <link rel="stylesheet" href="../../assets/css/responsive.css">
  <link rel="shortcut icon" href="../../assets/img/favicon.png" type="image/x-icon">
  <title>Dashboard - 30 Days Of Code</title>
@@ -23,12 +23,6 @@ if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
 <body class="flx col">
  <header class="flx row">
   <span>#30DaysOfCode</span>
-  <div class="techSymb flx row">
-   <img src="../../assets/img/htm.png">
-   <img src="../../assets/img/crly.png">
-   <img src="../../assets/img/prts.png">
-   <img src="../../assets/img/dsg.png">
-  </div>
   <div class="profile flx col">
     <img src="../../assets/img/profile.png">
     <ul class="options">
@@ -37,18 +31,16 @@ if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
   </div>
  </header>
  <div class="pageWrapper flx row">
-  <nav class="flx col closed" id="navPane">
-    <div class="hamBWrapper">
-      <div id="hamB" class="closed">   
-        <div class="a"></div> 
-        <div class="b"></div>
-        <div class="c"></div>
-      </div>
-    </div>
+  <nav class="flx col" id="navPane">
+  <div id="hamburger" class="flx col">
+      <div class="a"></div>
+      <div class="b"></div>
+      <div class="c"></div>
+ </div>
      <div class="flx col content">
        <ul class="linksContainer">
         <li class="flx row active">
-         <img src="../../assets/img/submsn.png">
+         <img src="../../assets/img/profileWT.png">
          <a href="../../user">User Dashboard</a>
         </li>
         <li class="flx row">
@@ -56,36 +48,34 @@ if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
          <a href='addnewtask.php'>Add New Tasks</a>
         </li>
         <li class="flx row">
-         <img src="../../assets/img/allTsk.png">
+         <img src="../../assets/img/task.png">
          <a href="super.php">All Tasks</a>
         </li>
         <li class="flx row">
-         <img src="../../assets/img/add.png">
+         <img src="../../assets/img/lock.png">
          <a href="../superadmin.php">Super Admin</a>
         </li>
         <li class="flx row">
-         <img src="../../assets/img/lead.png">
+         <img src="../../assets/img/podium.png">
          <a href="https://30daysofcode.xyz/leaderboard">Leaderboard</a>
         </li>
         <li class="flx row">
-         <img src="../../assets/img/tweet.png">
+         <img src="../../assets/img/twitter.png">
          <a href=" https://twitter.com/intent/tweet?url=https%3A%2F%2F30daysofcodes.xyz&via=codon&text=Hello%2C%20I%20just%20finished%20my%20task%20for%20....&hashtags=30DaysOfCode%2C%20ECX">Tweet</a>
-         <img class="external" style="float: right;" src="../assets/img/external.png" alt="">
         </li>
         <li class="flx row">
-         <img src="../../assets/img/wa.png">
+         <img src="../../assets/img/whatsapp.png">
          <a href="https://30daysofcode.xyz/whatsapp">Support group</a>
-         <img class="external" src="../assets/img/external.png" alt="">
         </li>
        </ul>
        <span id="email"><?=$_SESSION['login_user'];?></span>
      </div>   
    </nav>
    <div class="mainWrapper flx col" id="mainWrp">
-    <main>
-      <div class="flx row"><h1>Tasks</h1> </div>
-      <div class="mainCard">
-      <form method="POST">
+    <main class="flx col">
+      <form method="POST" class="flex col">
+        <legend>Tasks</legend>
+        <div class="fields-container flx col">
           <div class="field flx col">
             <label for="track">Track</label>
             <select name="track" value="">
@@ -106,10 +96,11 @@ if(isset( $_SESSION['login_user']) && $_SESSION['isAdmin'] == true){
           </div>
 
           <button id="submitTask" type="submit" name="submit">Submit</button>
+        </div>
         </form> 
      
-      </div >
-     </main>
+   
+     </main cla>
      <footer class="flx row"><span class="copyw">Copyright &copy; 30DaysOfCode 2020</span> <div><a href="">Privacy Policy</a><a href="">Terms &amp; Conditions</a></div></footer>
    </div>
  </div>
