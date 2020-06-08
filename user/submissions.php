b3fc7e7c843b3601ede7fa747bd74b7173381fe0
Sodiq Akinjobi <sodiq.akinjobi@gmail.com>
2020-06-08 17:39:08 +0100

    Update submissions.php

--- user/submissions.php
+++ user/submissions.php
@@ -107,7 +107,7 @@ if(isset( $_SESSION['login_user'])){
       <?php
       $u = $_SESSION['login_user'];
       $u = rtrim($u, '_');
-      $sql = "SELECT * FROM submissions WHERE `user` = '$u' AND `cohort` = '$cohort' ";
+      $sql = "SELECT * FROM submissions WHERE `user` = '$u'";
       $result = mysqli_query($conn, $sql);
       $count = mysqli_num_rows($result);
       
